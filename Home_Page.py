a7a2c410cb7f90ae69545ccf01b28d840d5b1999
tinayu777 <kristinayu777@gmail.com>
2024-11-26 21:36:16 -0500

    Update Home_Page.py

--- Home_Page.py
+++ Home_Page.py
@@ -13,7 +13,7 @@ st.subheader("Kristina Yu, Team Member 2")
 
 # Introduction
 # TODO: Write a quick description for all of your pages in this lab below, in the form:
-#       1. **Kristina's Portfolio**: A record of Kristina's Education and Job Experience
+#       1. **Page Name**: Description
 #       2. **Page Name**: Description
 #       3. **Page Name**: Description
 #       4. **Page Name**: Description
@@ -21,7 +21,7 @@ st.subheader("Kristina Yu, Team Member 2")
 st.write("""
 Welcome to our Streamlit Web Development Lab03 app! You can navigate between the pages using the sidebar to the left. The following pages are:
 
-1.
+1. **Kristina's Portfolio**: A record of Kristina's Education and Job Experience
 2.
 3.
 4.
